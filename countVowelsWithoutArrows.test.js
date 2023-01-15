da5b3b73cbfe8ffba66749dfd4d19446065d4c63
karthik0145 <karthikshriram5111@gmail.com>
2023-01-16 05:03:01 +0530

    chore: add test case to check functionality to count the number of vowles in a string without using arrow functions

--- countVowelsWithoutArrows.test.js
+++ countVowelsWithoutArrows.test.js
@@ -1,8 +1,8 @@
 const countVowels = require('./countVowels');
-
 test('testing countVowels withArrowfunction', () => {
   expect(countVowels.withoutArrow('aeiou')).toBe(5);
 });
+
 test('testing countVowels withArrow function', () => {
   expect(countVowels.withoutArrow('abcz')).toBe(1);
 });
