fd889377455e0bb4e706e93b3e4f6581d6e6de23
karthik0145 <karthikshriram5111@gmail.com>
2023-01-16 02:08:09 +0530

    chore: add test file to test countVowels with arrows

---
+++ countVowelsWithArrows.test.js
@@ -0,0 +1,14 @@
+const countVowels = require('./countVowels');
+
+test('testing countVowels withArrowfunction', () => {
+  expect(countVowels.withArrow('aeiou')).toBe(5);
+});
+test('testing countVowels withArrow function', () => {
+  expect(countVowels.withArrow('abcz')).toBe(1);
+});
+test('testing countVowels withArrow function', () => {
+  expect(countVowels.withArrow('hello')).toBe(2);
+});
+test('testing countVowels withArrow function', () => {
+  expect(countVowels.withArrow('codeacademy')).toBe(5);
+});
