d37392f9daa7a02725c46bd00b2aae4126ed9131
karthik0145 <karthikshriram5111@gmail.com>
2023-01-16 01:56:43 +0530

    fix: resolved module export error

--- replaceEveryCharacterWithFollowingCharacter.js
+++ replaceEveryCharacterWithFollowingCharacter.js
@@ -7,7 +7,20 @@ const replaceEveryCharacterWithFollowingCharacter=
             (character.charCodeAt(0)>=97 && character.charCodeAt(0)<=121)|| (character.charCodeAt(0)>=65 && character.charCodeAt(0)<=89)?asciValue=character.charCodeAt(0)+1:asciValue=asciValue;
             (character.charCodeAt(0)==122)?asciValue=97:asciValue=asciValue;
             (character.charCodeAt(0)==90)?asciValue=65:asciValue=asciValue;
-            //console.log(String.fromCharCode(asciValue));
+            asciValue!=-1?result=result.concat(String.fromCharCode(asciValue)):result=result;
+        }
+        let result='';
+        charArray=string.split('');
+        charArray.forEach(replaceFollowingCharacter);
+        return result;
+    },
+    withoutArrow:function (string)
+    {   const replaceFollowingCharacter=(character)=>
+        {
+            let asciValue=-1;
+            (character.charCodeAt(0)>=97 && character.charCodeAt(0)<=121)|| (character.charCodeAt(0)>=65 && character.charCodeAt(0)<=89)?asciValue=character.charCodeAt(0)+1:asciValue=asciValue;
+            (character.charCodeAt(0)==122)?asciValue=97:asciValue=asciValue;
+            (character.charCodeAt(0)==90)?asciValue=65:asciValue=asciValue;
             asciValue!=-1?result=result.concat(String.fromCharCode(asciValue)):result=result;
         }
         let result='';
@@ -18,4 +31,4 @@ const replaceEveryCharacterWithFollowingCharacter=
 }
 console.log(replaceEveryCharacterWithFollowingCharacter.withArrow('codeacademy'));
 console.log(replaceEveryCharacterWithFollowingCharacter.withArrow('lazyinterns'));
-module.export=replaceEveryCharacterWithFollowingCharacter;
+module.exports=replaceEveryCharacterWithFollowingCharacter;
