1d691b19ee4d9c5ee34c026668b00c3628a39b1b
karthik0145 <karthikshriram5111@gmail.com>
2023-01-16 03:06:31 +0530

    fix: resolved cases when input had space

--- changeCapitalization.js
+++ changeCapitalization.js
@@ -16,6 +16,7 @@ const changeCapitalization=
     withoutArrow:function (string)
     {function changeCase (character)
         {character=character.toString();
+            (character==' ')?result=result.concat(' '):result=result;
             (character.charCodeAt(0)>=65 && character.charCodeAt(0)<=90)?result=result.concat(character.toLowerCase().toString()):result=result;
             (character.charCodeAt(0)>=97 && character.charCodeAt(0)<=122)?result=result.concat(character.toUpperCase().toString()):result=result;
         }
