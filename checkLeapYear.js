09bbdf8835c02309cbbad543c8f89b97910d5637
karthik0145 <karthikshriram5111@gmail.com>
2023-01-15 19:25:17 +0530

    feat: add functionality to check leap year

---
+++ checkLeapYear.js
@@ -0,0 +1,22 @@
+
+
+const checkLeapYear=
+{
+    withArrow:(year)=>
+    {let result=false;
+        ((year%4==0 && year%100!=0) || (year%400==0))?result= true:result=false;
+        return result;
+    },
+    withoutArrow: function(year)
+    {
+        let result=false;
+        ((year%4==0 && year%100!=0) || (year%400==0))?result= true:result=false;
+        return result;
+    }
+
+}
+console.log(checkLeapYear.withArrow(1900));
+console.log(checkLeapYear.withArrow(1996));
+console.log(checkLeapYear.withArrow(2000));
+console.log(checkLeapYear.withArrow(2001));
+module.export=checkLeapYear;
