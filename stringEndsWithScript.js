b4f9edc1c22d625417831c6a83a495409ead4709
karthik0145 <karthikshriram5111@gmail.com>
2023-01-15 20:33:54 +0530

    feat: add functionality to check whether the string ends with Script and its length is greater than 6

---
+++ stringEndsWithScript.js
@@ -0,0 +1,14 @@
+const stringEndsWithScript=
+{
+    withArrow: (string)=>
+    {
+        return string.length>=6 && string.endsWith('Script');
+    },
+    withoutArrow: function(string)
+    {
+        return string.length>=6 && string.endsWith('Script');
+    }
+}
+console.log(stringEndsWithScript.withArrow('Script'));
+console.log(stringEndsWithScript.withoutArrow('Script'));
+module.import=stringEndsWithScript;
