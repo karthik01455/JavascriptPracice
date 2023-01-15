8c2a2a2b2135b28980c433bb4bfcde74c6758b85
karthik0145 <karthikshriram5111@gmail.com>
2023-01-16 02:51:13 +0530

    feat: add functionality to change capitalization of all letters

---
+++ changeCapitalization.js
@@ -0,0 +1,28 @@
+const changeCapitalization=
+{
+    withArrow: (string)=>
+    {const changeCase=(character)=>
+        {character=character.toString();
+            (character.charCodeAt(0)>=65 && character.charCodeAt(0)<=90)?result=result.concat(character.toLowerCase().toString()):result=result;
+            (character.charCodeAt(0)>=97 && character.charCodeAt(0)<=122)?result=result.concat(character.toUpperCase().toString()):result=result;
+        }
+        let result='';
+        stringArray= string.split('');
+        stringArray.forEach(changeCase);
+        return result;
+
+    },
+    withoutArrow:function (string)
+    {function changeCase (character)
+        {character=character.toString();
+            (character.charCodeAt(0)>=65 && character.charCodeAt(0)<=90)?result=result.concat(character.toLowerCase().toString()):result=result;
+            (character.charCodeAt(0)>=97 && character.charCodeAt(0)<=122)?result=result.concat(character.toUpperCase().toString()):result=result;
+        }
+        let result='';
+        stringArray= string.split('');
+        stringArray.forEach(changeCase);
+        return result;
+
+    }
+}
+module.exports=changeCapitalization;
