a4c0ade76bf9fcd5414f3eef6044f816e85160db
karthik0145 <karthikshriram5111@gmail.com>
2023-01-16 03:55:00 +0530

    feat: add functionality to concatenate two strings except their first character

---
+++ concatenateTwoStringsExceptTheirFirstCharacter.js
@@ -0,0 +1,30 @@
+const concatTwoStringsExceptTheirFirstCharacter=
+{
+    withArrow:(stringArrayInput)=>
+    {
+        const concatString=(stringArray)=>
+        {
+            result=stringArray[0].substring(1,stringArray[0].length).concat(stringArray[1].substring(1,stringArray[1].length));
+            return result;
+        }
+        let result='';
+        stringArrayInput.length==2?result=concatString(stringArrayInput):result=result;
+        return result;
+    },
+
+    withoutArrow: function (stringArrayInput)
+    {
+        function concatString(stringArray)
+        {
+            result=stringArray[0].substring(1,stringArray[0].length).concat(stringArray[1].substring(1,stringArray[1].length));
+            return result;
+        }
+        let result='';
+        stringArrayInput.length==2?result=concatString(stringArrayInput):result=result;
+        return result;
+    }
+    
+}
+console.log(concatTwoStringsExceptTheirFirstCharacter.withArrow(['array','math']));
+console.log(concatTwoStringsExceptTheirFirstCharacter.withoutArrow(['array','math']));
+module.exports=concatTwoStringsExceptTheirFirstCharacter;
